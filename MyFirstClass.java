47d47bc950030407a0cf9330160e22bbe58be135
RadostinGrigorov <grigorovradostin@gmail.com>
2025-07-27 15:32:45 +0300

    Add files via upload

---
+++ MyFirstClass.java
@@ -0,0 +1,11 @@
+package MyFirstPackage;
+
+public class MyFirstClass {
+
+    public static void main(String[] args){
+        String firstName = "Radostin";
+        String secondName = "Grigorov";
+        String lastName = "Grigorov";
+        System.out.println(firstName + " " + secondName + " " + lastName);
+    }
+}
