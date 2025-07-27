e8c8ba603040de7502e49e18fde9956beaf3b4ff
RadostinGrigorov <grigorovradostin@gmail.com>
2025-07-27 20:58:55 +0300

    Add files via upload

---
+++ Exercise2.java
@@ -0,0 +1,11 @@
+package MyFIrstPackage;
+
+public class Exercise2 {
+    public static void main(String[] args) {
+        int triangleSideA = 2;
+        int triangleSideB = 3;
+        int triangleSideC = 4;
+        int trianglePerimeter = triangleSideA + triangleSideB + triangleSideC;
+        System.out.println(trianglePerimeter);
+    }
+}
